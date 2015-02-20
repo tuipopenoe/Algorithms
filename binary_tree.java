19fcc1110d4e5c3846cea5ddd7a2e58db2cb79e7
Tui Popenoe <tuipopenoe@gmail.com>
2015-02-20 16:23:29 -0600

    Class methods

--- binary_tree.java
+++ binary_tree.java
@@ -26,15 +26,13 @@ public class BinaryTree{
         return(lookup(root, data));
     }
 
-
-
     // Recursive lookup -- given a node, recurse down searching for given data
     // O(log(n))
     private boolean lookup(Node node, int data){
         if(node == null){
             return(false);
         }
-        if(node == node.data){
+        if(dataoh == node.data){
             return(true);
         }
         else if(data < node.data){
@@ -66,5 +64,108 @@ public class BinaryTree{
         }
 
         return(node);
+    }
+
+    public int size(){
+        return(size(root));
+    }
+
+    private int size(Node node){
+        if(node == null){
+            return (0);
+        }
+        else{
+            return size(node.left) + 1 + size(node.right);
+        }
+    }
+
+    public int max_depth(){
+        return max_depth(root);
+    }
+
+    private int max_depth(Node node){
+        if(node == null){
+            return (0);
+        }
+        else{
+            ldepth = max_depth(node.left);
+            rdepth = max_depth(node.right);
+
+            return (Math.max(ldepth, rdepth) + 1);
+        }
+    }
+
+    public int min_value(){
+        return min_value(root);
+    }
+
+    private int min_value(Node node){
+        Node current = node;
+        while(current.left != null){
+            current = current.left;
+        }
+
+        return current.data;
+    }
+
+    public void print_inorder(){
+        return print_inorder(root);
+    }
+
+    private void print_inorder(Node node){
+        if(node == null){
+            return;
+        }
+        else{
+            print_inorder(node.left);
+            System.out.print(node.data);
+            print_inorder(node.right);
+        }
+    }
+
+    public void print_postorder(){
+        return print_postorder(root);
+    }
+
+    private void print_postorder(Node node){
+        if(node == null){
+            return;
+        }
+        else{
+            print_postorder(node.left);
+            print_postorder(node.right);
+            System.out.println(node.data);
+        }
+    }
+
+    public boolean has_path_sum(int sum){
+        return has_path_sum(root, sum);
+    }
+
+    private boolean has_path_sum(Node node, sum){
+        // return true if sum == 0 and child node is null
+        if(node == null){
+            return (sum == 0);
+        }
+        else{
+            int sub_sum = sum - node.data;
+            reutrn(has_path_sum(node.left, sub_sum) || 
+                   has_path_sum(node.right, sub_sum));
+        }
+    }
+
+    public void mirror(){
+        return mirror(root);
+    }
+
+    private void mirror(Node node){
+        if(node != null){
+            mirror(node.left);
+            mirror(node.right);
+            Node temp;
+            node.left = temp;
+            node.left = node.right;
+            node.right = temp;
+        }
     }
 }
